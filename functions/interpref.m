a55a93d82892108a2e225b6ec398b453fb9c357a
James A Desjardins <james.desjardins@brocku.ca>
2014-04-10 10:22:54 -0400

    add license information.

--- functions/interpref.m
+++ functions/interpref.m
@@ -1,12 +1,30 @@
-%This function reads coordinates files and interpolates
+%This function reads coordinate files and interpolates
 %data to these locations from the currently loaded data in EEGLAB. The
 %average of the interpolated channels is then calculated and subtracted
-%from each of channels in the currently loaded data in EEGLAB.
+%from each of channels in the currently loaded data in EEGLAB (Then the
+%interpolated channels are removed).
 
 %It is important that the sfp file containing the reference channel names
 %and coordinates 1- contains the same fiducials as the loaded coordinates
 %for the data field and 2- the channel names of the reference sites do not
 %match any of the names from the data channels.
+
+% Copyright (C) <2010>  <James Desjardins>
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 
 function EEG = interpref(EEG,coord_fname,varargin)
 
